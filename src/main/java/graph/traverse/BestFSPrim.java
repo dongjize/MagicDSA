8abce06039451791a464a6015dd0ea561b43d7d0
dongjize <justecommeca@163.com>
2017-10-10 23:32:42 +1100

    Prim-Jamik Algorithm

---
+++ src/main/java/graph/traverse/BestFSPrim.java
@@ -0,0 +1,32 @@
+package graph.traverse;
+
+import base.Iterator;
+import graph.Edge;
+import graph.Graph;
+import graph.Vertex;
+
+/**
+ * Description:（有向）带权图最小生成树的Prim-Jarnik算法
+ *
+ * @Author: dong
+ * @Date: 2017-10-10
+ * @Time: 23:15
+ */
+public class BestFSPrim extends BestFS {
+    public BestFSPrim(Graph g) {
+        super(g);
+    }
+
+    @Override
+    protected void updateDistanceAfter(Vertex v) {
+        for (Iterator it = v.outEdges(); it.hasNext(); ) {
+            Edge e = (Edge) it.getNext();
+            Vertex w = (Vertex) e.getVPosInV(1).getElem();
+            int weight = e.getWeight();
+            if (w.getDistance() > weight) {
+                w.setDistance(weight);
+                w.setBFSParent(v);
+            }
+        }
+    }
+}
